0f1d6f05385d3b8a5dc4296dbf94819df9ce5be6
Alessa-v-Scheidt <avscheidt49@gmail.com>
2022-03-26 00:09:02 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TodoElement from "./TodoElement";
 import {useState} from "react";
+import background from "./images/banana-g1d33f2aa5_1920.jpg";
 
 function App() {
   // const [] erstellt mir direkt eine Liste an Variablen, die sofort benannt werden
@@ -38,19 +39,21 @@ function App() {
   }
 
   return (
-    <div className="main-container">
-      <h1 className="header-text">
-        Todo - App
-      </h1>
-      {
-        // für jeden Text wird ein Element angelegt
-        todos.map((todo, index) => {
-          return <TodoElement todoText={todo} key={index} handleDelete={() => deleteTodoAt(index)} />
-        })
-      }
-      <div className="add-container">
-        <button onClick={handleAdd} className="add-button">+</button>
-        <input className="eingabefeld" type="text" value={newTodoText} onChange={handleChange} />
+    <div style={{ backgroundImage: `url(${background})` }}>
+      <div  className="main-container">
+        <h1 className="header-text">
+          Todo - App
+        </h1>
+        {
+          // für jeden Text wird ein Element angelegt
+          todos.map((todo, index) => {
+            return <TodoElement todoText={todo} key={index} handleDelete={() => deleteTodoAt(index)} />
+          })
+        }
+        <div className="add-container">
+          <button onClick={handleAdd} className="add-button">+</button>
+          <input className="eingabefeld" type="text" value={newTodoText} onChange={handleChange} />
+        </div>
       </div>
     </div>
   )
