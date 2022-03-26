f9045793ce325c914d68e80c7253c74e9f8304ff
Alessa-v-Scheidt <avscheidt49@gmail.com>
2022-03-26 01:23:57 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import TodoElement from "./TodoElement";
 import {useState} from "react";
-import background from "./images/banana-g1d33f2aa5_1920.jpg";
+//import background from "./images/banana-g1d33f2aa5_1920.jpg";
 import background2 from "./images/background-g653439c48_1920.jpg";
 
 function App() {
@@ -40,21 +40,19 @@ function App() {
   }
 
   return (
-    <div style={{ backgroundImage: `url(${background})` }}>
-      <div style={{ backgroundImage: `url(${background2})` }} className="main-container">
-        <h1 className="header-text">
-          Todo - App
-        </h1>
-        {
-          // für jeden Text wird ein Element angelegt
-          todos.map((todo, index) => {
-            return <TodoElement todoText={todo} key={index} handleDelete={() => deleteTodoAt(index)} />
-          })
-        }
-        <div className="add-container">
-          <button onClick={handleAdd} className="add-button">+</button>
-          <input className="eingabefeld" type="text" value={newTodoText} onChange={handleChange} />
-        </div>
+    <div style={{ backgroundImage: `url(${background2})` }} className="main-container">
+      <h1 className="header-text">
+        Todo - App
+      </h1>
+      {
+        // für jeden Text wird ein Element angelegt
+        todos.map((todo, index) => {
+          return <TodoElement todoText={todo} key={index} handleDelete={() => deleteTodoAt(index)} />
+        })
+      }
+      <div className="add-container">
+        <button onClick={handleAdd} className="add-button">+</button>
+        <input className="eingabefeld" type="text" value={newTodoText} onChange={handleChange} />
       </div>
     </div>
   )
